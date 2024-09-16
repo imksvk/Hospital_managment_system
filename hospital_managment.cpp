b57588c14601fe5b68ee40c339cc9a61f3d246e5
imksvk <keshavkumar123456789101112@gmail.com>
2024-09-17 01:55:06 +0530

    Complete First Part

--- hospital_managment.cpp
+++ hospital_managment.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 
 
+void addpatient(){
+    
+}
+
 int main(){
 
+    int choice ;
+    do{
+        cout<<"1. Add Patient"<<endl;
+        cout<<"2. Add Doctors"<<endl;
+        cout<<"3. Schedule Appointments"<<endl;
+        cout<<"4. View Patients list"<<endl;
+        cout<<"5. View Doctors list"<<endl;
+        cout<<"6. View Appointments"<<endl;
+        cout<<"0. Exit the App"<<endl;
+        cout<<"Enter Your Choice --> ";
+        cin>>choice;
+
+
+
+    }while(choice != 0);
 
 
     return 0;
