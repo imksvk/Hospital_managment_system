a81210ef3d303be63c244181be047ac1e3ae5754
imksvk <keshavkumar123456789101112@gmail.com>
2024-09-17 09:57:56 +0530

    Make doctor and patient class

--- hospital_managment.cpp
+++ hospital_managment.cpp
@@ -1,13 +1,73 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int pid;
+int did;
+
+class Patient{
+    public:
+        string name;
+        string gender;
+        int age;
+        int id;
+        Patient(string n, int a, string g){
+            id = pid++;
+            name = n;
+            age = a;
+            gender = g;
+        }
+};
+
+class Doctor{
+    public:
+        string name;
+        string gender;
+        int age;
+        int id;
+        Doctor(string n, int a, string g){
+            id = did++;
+            name = n;
+            age = a;
+            gender = g;
+        }
+};
+
+class Appointment{
+
+};
+
 
 void addpatient(){
-    
+
 }
 
-int main(){
+void adddoctor(){
 
+}
+void scheduleappointments(){
+
+}
+
+void viewdoctor(){
+
+}
+
+void viewpatient(){
+
+}
+
+void viewappointments(){
+
+
+}
+
+vector<Patient> allpatients;
+vector<Doctor> alldoctors;
+vector<Appointment> allappointments;
+
+int main(){
+    pid = 1;
+    did = 1;
     int choice ;
     do{
         cout<<"1. Add Patient"<<endl;
@@ -20,7 +80,33 @@ int main(){
         cout<<"Enter Your Choice --> ";
         cin>>choice;
 
-
+        switch(choice){
+            case 1:
+                addpatient();
+                break;
+            case 2:
+                adddoctor();
+                break;
+            case 3:
+                 scheduleappointments();
+                break;
+            case 4:
+                viewpatient();
+                break;
+            case 5:
+                viewdoctor();
+                break;
+            case 6:
+                viewappointments();
+                break;
+            case 0:
+                cout<<"Exiting the app";
+                exit(0);
+            default:
+                cout<<"Invalid Choice"<<endl;
+                cout<<endl<<endl<<endl<<endl;
+                
+        }
 
     }while(choice != 0);
 
